72f07bbe25a55ef0c66c18d5458cbb43d7e47cdd
rkothi <ranjigoud88@gmail.com>
2017-12-22 10:38:08 +0530

    secondtime

--- eclipseproject/src/eclipseproject/FirstProgram.java
+++ eclipseproject/src/eclipseproject/FirstProgram.java
@@ -5,6 +5,7 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Jai srimanarayana");
+		System.out.println("Hello");
 	}
 
 }
