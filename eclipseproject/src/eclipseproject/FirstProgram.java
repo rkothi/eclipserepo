fe63d0ee9305c4a012e5891bf7a66ad77c118fab
rkothi <ranjigoud88@gmail.com>
2017-12-22 10:49:12 +0530

    negative iteration

--- eclipseproject/src/eclipseproject/FirstProgram.java
+++ eclipseproject/src/eclipseproject/FirstProgram.java
@@ -6,7 +6,7 @@ public class FirstProgram {
 		// TODO Auto-generated method stub
 		System.out.println("Jai srimanarayana");
 		System.out.println("Hello");
-		for(int i =0;i<10;i++)
+		for(int i =0;i<10;i--)
 		{
 			System.out.println("Hello..." + i);
 		}
